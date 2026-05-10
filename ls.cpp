be1ac228047dfffabdba8c11e695dc3bf5de3a11
Aditya <adigithub354@gmail.com>
2026-05-10 12:46:59 +0530

    added option for colored output [-C]

--- ls.cpp
+++ ls.cpp
@@ -6,10 +6,16 @@
 namespace fs = std::filesystem;
 static constexpr std::string_view dot { "." };
 
+struct colors {
+    static constexpr std::string_view YELLOW { "\e[0;33m" };
+    static constexpr std::string_view BLUE { "\e[0;34m" };
+};
+
 struct PrintOptions {
     fs::path path_to_print { fs::current_path() };
     bool user_path { false };
     bool show_hidden { false };
+    bool use_colors { false };
 };
 
 PrintOptions create_print_options(std::span<char*> arguments) {
@@ -17,8 +23,8 @@ PrintOptions create_print_options(std::span<char*> arguments) {
     size_t i { 1 };
     while(i < arguments.size()) {
         std::string argument { arguments[i++] };
-        if(argument == "-a")
-            print_options.show_hidden = true;
+        if(argument == "-a") print_options.show_hidden = true;
+        else if(argument == "-C") print_options.use_colors = true;
         else if(!fs::exists(argument)) {
             std::cerr << "not a valid path!\n";
             continue;
@@ -39,16 +45,17 @@ class Printer {
         {}
 
         void print() {
-            std::println("print function is called");
             for(const fs::directory_entry& entry : fs::directory_iterator(m_options.path_to_print)) {
                 if(entry.is_directory()) {
                     if(!m_options.show_hidden && entry.path().filename().string().starts_with(dot))
                         continue;
-                    std::println("Directory: {}", entry.path().filename().string());
+                    if(m_options.use_colors) std::println("{}{}", colors::BLUE, entry.path().filename().string());
+                    else std::println("{}", entry.path().filename().string());
                 } else {
                     if(!m_options.show_hidden && entry.path().filename().string().starts_with(dot))
                         continue;
-                    std::println("File: {}", entry.path().filename().string());
+                    if(m_options.use_colors) std::println("{}{}", colors::YELLOW, entry.path().filename().string());
+                    else std::println("{}", entry.path().filename().string());
                 }
             }
         }
